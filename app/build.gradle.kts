25c222cdcd0a20090f6ca939582af3ab52a7e40a
Matheus Cardoso <matheuslive@live.com>
2024-09-02 16:29:43 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ val keystoreProperties = if (keystorePropertiesFile.exists() && keystoreProperti
 
 android {
     namespace = "io.github.a13e300.tools"
-    compileSdk = 34
+    compileSdk = 35
     signingConfigs {
         if (keystoreProperties != null) {
             create("release") {
@@ -37,7 +37,7 @@ android {
     defaultConfig {
         applicationId = "io.github.a13e300.tools.stethox"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 2
         versionName = "1.0.2"
         externalNativeBuild {
